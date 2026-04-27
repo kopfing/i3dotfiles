43031191171d5d1dbbaff1a50a2f12d34d75c4aa
kopfing <janmoser@kopfing6.at>
2026-04-27 18:57:46 +0200

    ahm

--- .scripts/verticalMon.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DVI-D-0 --mode 1920x1080 --pos 1920x0 --rotate left --output HDMI-0 --off --output DP-0 --primary --mode 1920x1080 --pos 0x637 --rotate normal --output DP-1 --off
